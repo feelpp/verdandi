e3c7a061a9338dc4cbe0c57604ef488421552e92
Claire Mouton <claire.mouton@inria.fr>
2009-07-28 10:22:00 +0200

    Added a test program for optimal interpolation with sparse matrices.

---
+++ example/optimal_interpolation_sparse.cpp
@@ -0,0 +1,55 @@
+#define SELDON_WITH_CBLAS
+#define SELDON_WITH_LAPACK
+#define SELDON_DEBUG_LEVEL_4
+#define SELDON_WITH_ABORT
+
+#include "Verdandi.hxx"
+using namespace Verdandi;
+
+#include "OptimalInterpolation.cxx"
+#include "DiagonalSparseObservationManager.cxx"
+#include "ShallowWater.cxx"
+#include "OutputSaver.cxx"
+#include "newran.h"
+
+int main(int argc, char** argv)
+{
+
+    TRY;
+
+    if (argc != 2)
+    {
+        string mesg  = "Usage:\n";
+        mesg += string("  ") + argv[0] + " [configuration file]";
+        cout << mesg << endl;
+        return 1;
+    }
+
+    typedef double real;
+    typedef ShallowWater<real> ClassModel;
+    typedef OptimalInterpolation<real, ClassModel,
+        DiagonalSparseObservationManager<real> > ClassOptimalInterpolation;
+
+    ClassOptimalInterpolation driver(argv[1]);
+
+    OutputSaver<real, ClassOptimalInterpolation> output_saver(argv[1],
+                                                              driver);
+
+    driver.Initialize(argv[1]);
+    output_saver.Initialize(argv[1], driver);
+
+    while (!driver.HasFinished())
+    {
+        driver.InitializeStep();
+        output_saver.InitializeStep();
+        driver.Forward();
+        output_saver.Save(driver);
+        driver.Analyze();
+        output_saver.Save(driver);
+    }
+
+    END;
+
+    return 0;
+
+}
