f562996e502f89d64e0575a64e143db9150cdd99
Vivien Mallet <Vivien.Mallet@inria.fr>
2009-11-26 11:30:29 +0100

    Initial import of 'ForwardDriver'.

---
+++ method/ForwardDriver.cxx
@@ -0,0 +1,178 @@
+// Copyright (C) 2009 INRIA
+// Author(s): Vivien Mallet, Claire Mouton
+//
+// This file is part of the data assimilation library Verdandi.
+//
+// Verdandi is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2.1 of the License, or (at your option)
+// any later version.
+//
+// Verdandi is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Verdandi. If not, see http://www.gnu.org/licenses/.
+
+
+#ifndef VERDANDI_FILE_METHOD_FORWARDDRIVER_CXX
+
+
+#include "ForwardDriver.hxx"
+
+
+namespace Verdandi
+{
+
+
+    ////////////////////////////////
+    // CONSTRUCTOR AND DESTRUCTOR //
+    ////////////////////////////////
+
+
+    //! Main constructor.
+    /*! Builds the driver and reads option keys in the configuration file.
+      \param[in] configuration_file configuration file.
+    */
+    template <class ClassModel>
+    ForwardDriver<ClassModel>::ForwardDriver(string configuration_file):
+        model_(configuration_file), iteration_(-1)
+    {
+        GetPot configuration_stream(configuration_file);
+
+        /*** Initializations ***/
+
+        MessageHandler::AddRecipient("model", model_,
+                                     ClassModel::StaticMessage);
+        MessageHandler::AddRecipient("driver", *this,
+                                     ForwardDriver::StaticMessage);
+
+        /*** Display options ***/
+
+        configuration_stream.set_prefix("display/");
+        // Should the iteration be displayed on screen?
+        configuration_stream.set("Show_iteration", show_iteration_);
+        // Should the date be displayed on screen?
+        configuration_stream.set("Show_date", show_date_);
+    }
+
+
+    //! Destructor.
+    template <class ClassModel>
+    ForwardDriver<ClassModel>::~ForwardDriver()
+    {
+    }
+
+
+    /////////////
+    // METHODS //
+    /////////////
+
+
+    //! Initializes the simulation.
+    /*! Initializes the model.
+      \param[in] configuration_file configuration file to be given to the
+      model initialization method.
+    */
+    template <class ClassModel>
+    void ForwardDriver<ClassModel>::Initialize(string configuration_file)
+    {
+        MessageHandler::Send(*this, "all", "::Initialize begin");
+
+        if (show_date_)
+            Logger::StdOut(*this, "Date: " + to_str(model_.GetDate()));
+        else
+            Logger::Log<-3>(*this,
+                            "Date: " + to_str(model_.GetDate()));
+        if (show_iteration_)
+            Logger::StdOut(*this, "Initialization");
+        else
+            Logger::Log<-3>(*this, "Initialization");
+
+        model_.Initialize(configuration_file);
+        iteration_ = 0;
+
+        MessageHandler::Send(*this, "all", "::Initialize end");
+    }
+
+
+    //! Initializes the model before a time step.
+    template <class ClassModel>
+    void ForwardDriver<ClassModel>::InitializeStep()
+    {
+        MessageHandler::Send(*this, "all", "::InitializeStep begin");
+
+        model_.InitializeStep();
+
+        MessageHandler::Send(*this, "all", "::InitializeStep end");
+    }
+
+
+    //! Performs a step forward without optimal interpolation.
+    template <class ClassModel>
+    void ForwardDriver<ClassModel>::Forward()
+    {
+        MessageHandler::Send(*this, "all", "::Forward begin");
+
+        if (show_date_)
+            Logger::StdOut(*this, "Date: " + to_str(model_.GetDate()));
+        else
+            Logger::Log<-3>(*this,
+                            "Date: " + to_str(model_.GetDate()));
+        if (show_iteration_)
+            Logger::StdOut(*this, "Iteration " + to_str(iteration_) + " -> "
+                           + to_str(iteration_ + 1));
+        else
+            Logger::Log<-3>(*this, "Iteration " + to_str(iteration_) + " -> "
+                            + to_str(iteration_ + 1));
+
+        model_.Forward();
+        iteration_++;
+
+        MessageHandler::Send(*this, "model", "forecast");
+
+        MessageHandler::Send(*this, "all", "::Forward end");
+    }
+
+
+    //! Checks whether the model has finished.
+    /*!
+      \return True if no more data assimilation is required, false otherwise.
+    */
+    template <class ClassModel>
+    bool ForwardDriver<ClassModel>::HasFinished() const
+    {
+        return model_.HasFinished();
+    }
+
+
+    //! Returns the model.
+    /*!
+      \return The model.
+    */
+    template <class ClassModel>
+    const ClassModel&
+    ForwardDriver<ClassModel>::GetModel() const
+    {
+        return model_;
+    }
+
+
+    //! Returns the name of the class.
+    /*!
+      \return The name of the class.
+    */
+    template <class ClassModel>
+    string ForwardDriver<ClassModel>::GetName() const
+    {
+        return "ForwardDriver";
+    }
+
+
+} // namespace Verdandi.
+
+
+#define VERDANDI_FILE_METHOD_FORWARDDRIVER_CXX
+#endif
---
+++ method/ForwardDriver.hxx
@@ -0,0 +1,77 @@
+// Copyright (C) 2009 INRIA
+// Author(s): Vivien Mallet, Claire Mouton
+//
+// This file is part of the data assimilation library Verdandi.
+//
+// Verdandi is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2.1 of the License, or (at your option)
+// any later version.
+//
+// Verdandi is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Verdandi. If not, see http://www.gnu.org/licenses/.
+
+
+#ifndef VERDANDI_FILE_METHOD_FORWARDDRIVER_HXX
+
+
+namespace Verdandi
+{
+
+
+    ///////////////////
+    // FORWARDDRIVER //
+    ///////////////////
+
+
+    //! This class simply performs a forward simulation.
+    template <class ClassModel>
+    class ForwardDriver: public VerdandiBase
+    {
+
+    protected:
+
+        //! Underlying model.
+        ClassModel model_;
+
+        //! Iteration.
+        int iteration_;
+
+        /*** Configuration ***/
+
+        //! Should the iterations be displayed?
+        bool show_iteration_;
+        //! Should the current date be displayed?
+        bool show_date_;
+
+    public:
+
+        /*** Constructor and destructor ***/
+
+        ForwardDriver(string configuration_file);
+        ~ForwardDriver();
+
+        /*** Methods ***/
+
+        void Initialize(string configuration_file);
+        void InitializeStep();
+        void Forward();
+
+        bool HasFinished() const;
+
+        // Access methods.
+        const ClassModel& GetModel() const;
+        string GetName() const;
+    };
+
+
+} // namespace Verdandi.
+
+
+#define VERDANDI_FILE_METHOD_FORWARDDRIVER_HXX
+#endif
