816395f453464b24865af2948797d256f912b26b
Marc Fragu <marc.fragu@inria.fr>
2009-12-03 16:32:28 +0100

    Initial import of "example/clamped_bar/optimal_interpolation.cpp".

---
+++ example/clamped_bar/optimal_interpolation.cpp
@@ -0,0 +1,67 @@
+#define SELDON_DEBUG_LEVEL_4
+#define SELDON_WITH_ABORT
+#define SELDON_WITH_CBLAS
+#define SELDON_WITH_LAPACK
+
+//#define SELDON_WITH_MUMPS
+#define SELDON_WITH_SUPERLU
+//#define SELDON_WITH_UMFPACK
+
+//#define SELDON_WITH_MPI
+
+#define VERDANDI_WITH_ABORT
+
+#define VERDANDI_WITH_DIRECT_SOLVER
+
+#if !defined(SELDON_WITH_UMFPACK) && !defined(SELDON_WITH_SUPERLU)      \
+    && !defined(SELDON_WITH_MUMPS) && defined(VERDANDI_WITH_DIRECT_SOLVER)
+#define SELDON_WITH_SUPERLU
+#endif
+
+#define VERDANDI_SPARSE
+
+#include "Verdandi.hxx"
+
+using namespace Verdandi;
+#include "seldon/SeldonSolver.hxx"
+
+#include "OptimalInterpolation.cxx"
+#include "LinearObservationManager.cxx"
+#include "ClampedBar.cxx"
+#include "newran/newran.h"
+
+
+int main(int argc, char** argv)
+{
+
+    TRY;
+
+    if (argc != 2)
+    {
+        string mesg  = "Usage:\n";
+        mesg += string("  ") + argv[0] + " [configuration file]";
+        cout << mesg << endl;
+        return 1;
+    }
+
+    typedef double real;
+
+    OptimalInterpolation<real, ClampedBar<real>,
+        LinearObservationManager<real> > driver(argv[1]);
+
+    driver.Initialize(argv[1]);
+
+    while (!driver.HasFinished())
+    {
+        driver.InitializeStep();
+
+        driver.Forward();
+
+        driver.Analyze();
+    }
+
+    END;
+
+    return 0;
+
+}
