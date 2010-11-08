718d53731137673c4e2796a1b0d62058e395bfe9
Marc Fragu <marc.fragu@inria.fr>
2010-11-08 14:15:49 +0100

    Initial import of the documentation for 'ReducedOrderUnscentedKalmanFilter'.

--- doc/guide/header.php
+++ doc/guide/header.php
@@ -68,7 +68,8 @@ or basename($_SERVER['REQUEST_URI'], ".php") == "overview")
 <?php if (basename($_SERVER['REQUEST_URI'], ".php") == "assimilation_methods"
 or basename($_SERVER['REQUEST_URI'], ".php") == "optimal_interpolation"
 or basename($_SERVER['REQUEST_URI'], ".php") == "extended_kalman_filter"
-or basename($_SERVER['REQUEST_URI'], ".php") == "unscented_kalman_filter")
+or basename($_SERVER['REQUEST_URI'], ".php") == "unscented_kalman_filter"
+or basename($_SERVER['REQUEST_URI'], ".php") == "reduced_order_unscented_kalman_filter")
 {
   echo '<ul class="navsubul"> <li class="jelly">';
   HL($file, "optimal_interpolation", "Optimal Interpolation");
@@ -78,6 +79,9 @@ or basename($_SERVER['REQUEST_URI'], ".php") == "unscented_kalman_filter")
   echo '</li>';
   echo '<li class="jelly">';
   HL($file, "unscented_kalman_filter", "Unscented Kalman Filter");
+  echo '</li>';
+  echo '<li class="jelly">';
+  HL($file, "reduced_order_unscented_kalman_filter", "Reduced Order Unscented Kalman Filter");
   echo '</li> </ul>';
 } ?>
 
