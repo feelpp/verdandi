8c96abd510e00ca15243dc42bbeae255cee93177
LBer <lberti21@gmail.com>
2022-01-11 14:43:29 +0100

    add other assimilation methods to quadratic model example #1

--- example/quadratic_model/configuration/perturbation_manager.lua
+++ example/quadratic_model/configuration/perturbation_manager.lua
@@ -16,6 +16,22 @@ perturbation_manager = {
       -- seed_directory = os.getenv("HOME") .. "/.newran/"
 
    },
+   random = {
+
+      -- Choose between
+      -- * "time" for machine time,
+      -- * "number" for a fixed seed value between 0 and 1, and
+      -- * "directory" to use a Newran directory which stores the seeds.
+      seed_type = "number",
+
+      -- If 'seed_type' is set to "number", choose a number in ]0, 1[.
+      seed_number = 0.4,
+
+      -- If 'seed_type' is set to "directory", put the path to the Newran
+      -- directory. It must end with a slash.
+      -- seed_directory = os.getenv("HOME") .. "/.newran/"
+
+   },
 
    output = {
 
